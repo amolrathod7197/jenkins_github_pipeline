4c57c33083ad87205e0c2769b6efeb7c50da0b36
Amol <amol.rathod@techbulls.com>
2025-09-21 10:54:38 +0530

    Initial commit: Spring Boot demo with Jenkins pipeline

---
+++ src/main/java/com/arcoding/pipeline/DemoController.java
@@ -0,0 +1,12 @@
+package com.arcoding.pipeline;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Jenkins CI/CD Demo!";
+    }
+} 
